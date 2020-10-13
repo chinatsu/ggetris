46d2ffc7828be598e13f687c6341031dea121b8b
Kent Daleng <kent.stefan.daleng@nav.no>
2020-10-14 00:54:22 +0200

    mess w/ kick table

--- src/tetris/piece.rs
+++ src/tetris/piece.rs
@@ -119,11 +119,13 @@ impl Piece {
             Point{x: -1, y: 0},
             Point{x: 1, y: 0},
             Point{x: 0, y: 1},
-            Point{x: 0, y: -1},
             Point{x: 1, y: 1},
             Point{x: -1, y: 1},
             Point{x: -2, y: 0},
             Point{x: 2, y: 0},
+            Point{x: 1, y: 2},
+            Point{x: -1, y: 2},
+            Point{x: 0, y: -1},
             Point{x: -1, y: -1},
             Point{x: 1, y: -1}
         ];
